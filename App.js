26b6bea8690d43ff1fbd4e0ec12a278536962dc7
Wido Wicaksono <widowicak@gmail.com>
2021-02-11 21:10:42 +0700

    basic done

--- App.js
+++ App.js
@@ -1,18 +1,45 @@
-import {Button, Input} from 'native-base';
+import {Body, Button, Card, CardItem, Icon, Input, Item} from 'native-base';
 import React, {useState} from 'react';
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import {Dimensions, FlatList, StyleSheet, Text, View} from 'react-native';
 
 function App() {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(['halo', 'halo']);
   const [input, setInput] = useState('');
+
+  const renderCard = ({item}) => {
+    console.log(input);
+    return (
+      <View>
+        <Card>
+          <CardItem>
+            <Body>
+              <Text>{item}</Text>
+            </Body>
+          </CardItem>
+        </Card>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.mainContainer}>
       <View style={styles.topContainer}>
-        <Text>TodoApp</Text>
+        <FlatList data={list} renderItem={renderCard} />
       </View>
       <View style={styles.bottomContainer}>
-        <Input placeholder="Underline Textbox" />
-        <Button block info>
+        <Item>
+          <Input
+            placeholder="What would you do, today?"
+            style={styles.inputStyle}
+            onChangeText={(e) => setInput(e)}
+            value={input}
+          />
+        </Item>
+        <Button
+          block
+          info
+          style={styles.btnStyle}
+          onPress={() => setList([...list, input])}>
           <Text style={styles.fontStyle}>Add</Text>
         </Button>
       </View>
@@ -25,22 +52,37 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: 10,
+    backgroundColor: '#a0c1b8',
   },
   bottomContainer: {
     flex: 1,
-    paddingTop: 50,
-    paddingBottom: 50,
+    marginTop: 50,
+    height: Dimensions.get('screen').height / 15,
+    width: Dimensions.get('screen').width,
     justifyContent: 'center',
-    alignSelf: 'center',
+    alignItems: 'center',
   },
   topContainer: {
     flex: 1,
+    width: Dimensions.get('screen').width / 1.5,
   },
   fontStyle: {
     color: 'white',
     fontSize: 20,
     fontWeight: '700',
+    textAlign: 'center',
+  },
+  btnStyle: {
+    marginHorizontal: 10,
+    marginTop: 20,
+  },
+  inputStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  cardStyle: {
+    marginVertical: 5,
   },
 });
 
