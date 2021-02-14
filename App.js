401f6d4c71098c68c6475a827e8700a0179909b6
Wido Wicaksono <widowicak@gmail.com>
2021-02-14 16:28:18 +0700

    todoapp done

--- App.js
+++ App.js
@@ -1,31 +1,107 @@
-import {Body, Button, Card, CardItem, Icon, Input, Item} from 'native-base';
+import {Body, Button, Card, CardItem, Input, Item} from 'native-base';
 import React, {useState} from 'react';
-import {Dimensions, FlatList, StyleSheet, Text, View} from 'react-native';
+import {
+  Dimensions,
+  FlatList,
+  Keyboard,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+} from 'react-native';
 
 function App() {
   const [list, setList] = useState(['halo', 'halo']);
-  const [input, setInput] = useState('');
+  const [listDone, setListDone] = useState(['done test']);
+  const [input, setInput] = useState();
 
-  const renderCard = ({item}) => {
-    console.log(input);
+  const renderCard = (item, index) => {
     return (
-      <View>
-        <Card>
-          <CardItem>
-            <Body>
-              <Text>{item}</Text>
-            </Body>
-          </CardItem>
-        </Card>
-      </View>
+      <TouchableOpacity>
+        <View style={styles.cardStyle}>
+          <Card>
+            <CardItem>
+              <Body style={styles.cardBody}>
+                <Text style={styles.cardText}>{item}</Text>
+                <View style={styles.cardBtn}>
+                  <Button
+                    bordered
+                    danger
+                    style={styles.deleteBtn}
+                    onPress={() => deleteBtn(index)}>
+                    <Text style={styles.deleteText}>Delete</Text>
+                  </Button>
+                  <Button
+                    bordered
+                    success
+                    style={styles.doneBtn}
+                    onPress={() => doneBtn(index)}>
+                    <Text style={styles.doneText}>Done</Text>
+                  </Button>
+                </View>
+              </Body>
+            </CardItem>
+          </Card>
+        </View>
+      </TouchableOpacity>
     );
+  };
+
+  const renderCardDone = ({item}) => {
+    return (
+      <TouchableOpacity>
+        <View style={styles.cardStyle} key={item}>
+          <Card>
+            <CardItem>
+              <Body style={styles.cardBody}>
+                <Text style={styles.cardTextDone}>{item}</Text>
+              </Body>
+            </CardItem>
+          </Card>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  const addTodo = () => {
+    setList([...list, input]);
+    setInput(null);
+    Keyboard.dismiss();
+  };
+
+  const doneBtn = (index) => {
+    let done = list.slice(index, index + 1);
+    setListDone([...listDone, done]);
+    let listCopy = [...list];
+    listCopy.splice(index, 1);
+    setList(listCopy);
+  };
+
+  const deleteBtn = (index) => {
+    let listCopy = [...list];
+    listCopy.splice(index, 1);
+    setList(listCopy);
   };
 
   return (
     <View style={styles.mainContainer}>
-      <View style={styles.topContainer}>
-        <FlatList data={list} renderItem={renderCard} />
-      </View>
+      <ScrollView style={styles.topContainer}>
+        <View>
+          <Text style={styles.sectionTitle}>Today's to do</Text>
+          <FlatList
+            data={list}
+            renderItem={({item, index}) => {
+              return renderCard(item, index);
+            }}
+            style={styles.flatList}
+          />
+        </View>
+        <View style={styles.todoDoneWrapper}>
+          <Text style={styles.todoDone}>To do done</Text>
+          <FlatList data={listDone} renderItem={renderCardDone} />
+        </View>
+      </ScrollView>
       <View style={styles.bottomContainer}>
         <Item>
           <Input
@@ -33,13 +109,12 @@ function App() {
             style={styles.inputStyle}
             onChangeText={(e) => setInput(e)}
             value={input}
+            onSubmitEditing={addTodo}
+            autoCapitalize="sentences"
+            // onKeyPress={(e) => (e.nativeEvent.key == 'Enter' ? addTodo : null)}
           />
         </Item>
-        <Button
-          block
-          info
-          style={styles.btnStyle}
-          onPress={() => setList([...list, input])}>
+        <Button block info style={styles.btnStyle} onPress={addTodo}>
           <Text style={styles.fontStyle}>Add</Text>
         </Button>
       </View>
@@ -52,11 +127,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#a0c1b8',
+    backgroundColor: '#E8EAED',
   },
   bottomContainer: {
-    flex: 1,
-    marginTop: 50,
+    flex: 0.3,
+    // marginTop: 50,
     height: Dimensions.get('screen').height / 15,
     width: Dimensions.get('screen').width,
     justifyContent: 'center',
@@ -64,7 +139,9 @@ const styles = StyleSheet.create({
   },
   topContainer: {
     flex: 1,
-    width: Dimensions.get('screen').width / 1.5,
+    width: Dimensions.get('screen').width,
+    paddingTop: 40,
+    marginBottom: 10,
   },
   fontStyle: {
     color: 'white',
@@ -73,7 +150,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   btnStyle: {
-    marginHorizontal: 10,
+    marginHorizontal: 30,
     marginTop: 20,
   },
   inputStyle: {
@@ -82,7 +159,65 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   cardStyle: {
-    marginVertical: 5,
+    marginHorizontal: 30,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingLeft: 30,
+  },
+  cardBody: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  cardText: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  cardTextDone: {
+    fontSize: 20,
+    fontWeight: '500',
+    textDecorationLine: 'line-through',
+  },
+  cardBtn: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  deleteBtn: {
+    marginHorizontal: 5,
+    width: 55,
+  },
+  deleteText: {
+    textAlign: 'center',
+    fontWeight: '500',
+    marginHorizontal: 6,
+    color: 'red',
+  },
+  doneBtn: {
+    width: 50,
+  },
+  doneText: {
+    textAlign: 'center',
+    marginHorizontal: 7,
+    fontSize: 15,
+    color: 'green',
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  todoDone: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingLeft: 30,
+  },
+  todoDoneWrapper: {
+    marginBottom: 100,
   },
 });
 
